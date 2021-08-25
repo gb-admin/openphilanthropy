a8f76e6aeb836ee09b58907f760fe3bf632da0cd
Ewart Xia <hello@ewart.dev>
2021-08-25 15:06:54 -0700

    Task #115 - fixing default template for pages

--- wp-content/themes/oph/page.php
+++ wp-content/themes/oph/page.php
@@ -13,6 +13,9 @@
  */
 
 get_header();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 	<main id="primary" class="site-main">
@@ -21,7 +24,7 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'part/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
