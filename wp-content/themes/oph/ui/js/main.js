2d2a955ff7a33eb758c272094e13e7dbbc2eb8c2
Ewart Xia <hello@ewart.dev>
2021-08-25 10:39:13 -0700

    Task #124 - Please change this  button to  say "See Open Positions" and have it anchor to the open positions tier above.

--- wp-content/themes/oph/ui/js/main.js
+++ wp-content/themes/oph/ui/js/main.js
@@ -1141,4 +1141,19 @@ jQuery(function($) {
   $(window).on('resize', function() {
     setTimeout(careersSignupFormSetup, 300);
   });
+
+  // Adds smooth scroll to anchor tags
+  $('a[href*="#"]:not([href="#"])').on('click', function() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+		  	$('html,body').animate({
+				scrollTop: target.offset().top
+		  	}, 1000);
+		  	return false;
+		}
+	}
+  });
+
 });
