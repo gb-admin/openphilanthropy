53308e605a105870d9bd3430ef3f7869ff947080
Ewart Xia <hello@ewart.dev>
2021-09-10 11:39:34 -0700

    Task #178 - Implemented cookies to remember the sidebar visibility toggle settings per page. Added cache busting version on main.js

--- wp-content/themes/oph/inc/asset.php
+++ wp-content/themes/oph/inc/asset.php
@@ -13,9 +13,11 @@ function enqueue_asset() {
 
 	wp_enqueue_script( 'jquery-bundle-script', get_template_directory_uri() . '/ui/js/jquery.bundle.js', array( 'jquery' ), '', true );
 
-	wp_enqueue_script( 'main-script', get_template_directory_uri() . '/ui/js/main.js', array( 'jquery' ), '', true );
+	wp_enqueue_script( 'main-script', get_template_directory_uri() . '/ui/js/main.js', array( 'jquery', 'js-cookie' ), '1.0.0', true );
 
 	wp_enqueue_script( 'skip-link-focus-fix', get_template_directory_uri() . '/ui/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'js-cookie', get_template_directory_uri() . '/ui/js/js.cookie.min.js', array(), '3.0.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
