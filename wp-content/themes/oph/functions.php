c4876240271db986cda32a6b0ee2af93af224d15
Ewart Xia <hello@ewart.dev>
2021-12-08 17:37:43 -0800

    Task #183 - Switch from static grant_db.csv generation to dynamic
    1. Added rewrite endpoint for grants/download-spreadsheet and updated the frontend's anchor's href
    2. Removed the CSV on page generation and opted to save the grant query in $_SESSION
    3. Lightly modified the existing csv generation script to output to browser (prompt to save) instead of a static file
    4. Tucked everything under oph > inc > grants_db.php
    
    ACTION needed: flush rewrite rules

--- wp-content/themes/oph/functions.php
+++ wp-content/themes/oph/functions.php
@@ -53,6 +53,7 @@ require get_template_directory() . '/inc/shortcode.php';
 require get_template_directory() . '/inc/sanitize-functions.php';
 require get_template_directory() . '/inc/metabox.php';
 require get_template_directory() . '/inc/helper-functions.php';
+require get_template_directory() . '/inc/grants_db.php';
 
 // Used for data migration
 include get_template_directory() . '/inc/onetime-script.php';
