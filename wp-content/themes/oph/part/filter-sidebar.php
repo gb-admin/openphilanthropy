50de47e123567908d294e8aa408196050c903347
Ash Saraga <ashsaraga@gmail.com>
2021-12-17 14:55:14 -0500

    Revert "Revert "Task #183 - Switch from static grant_db.csv generation to dynamic""

--- wp-content/themes/oph/part/filter-sidebar.php
+++ wp-content/themes/oph/part/filter-sidebar.php
@@ -201,7 +201,7 @@
 
 				<?php if ( is_page('grants') ) : ?>
 				<div class="sidebar-filter__button">
-					<a class="button" href="<?php echo get_stylesheet_directory_uri() . '/grants_db.csv'; ?>" download="grants_db.csv">Download Spreadsheet</a>
+					<a class="button" href="<?php echo home_url() . '/grants/download-spreadsheet/' ?>">Download Spreadsheet</a>
 				</div>
 				<?php endif; ?>
 			</nav>
