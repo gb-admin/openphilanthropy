460b90b8e38ec3ef542ffa2d3ad3fb9cec0f2aea
Ash Saraga <ashsaraga@gmail.com>
2022-01-11 17:04:52 -0500

    Creating Basic Sections Template && Updating Basic Text Template

--- wp-content/themes/oph/template/basic-text-template.php
+++ wp-content/themes/oph/template/basic-text-template.php
@@ -7,91 +7,21 @@
 
 	get_header();
 
-	$content_sections = get_field( 'content_sections' );
 	$footnotes = get_field( 'footnotes' );
 ?>
 
 <?php get_template_part( 'part/page', 'header' ); ?>
 
-<div class="content-table-of-contents">
+<div class="content-basic-text">
 	<div class="wrap">
-		<div class="content-table-of-contents__main">
-			<?php if ( $content_sections ) : ?>
+		<div class="content-basic-text__main">
+			<?php the_content(); ?>
 
-				<?php
-					$pagenav_sections = [];
-
-					foreach ( $content_sections as $k => $i ) {
-						$section_array = array(
-							'title' => $i['section_title'],
-							'sub_sections' => []
-						);
-
-						if ( isset( $i['content'] ) ) {
-							foreach ( $i['content'] as $c ) {
-								if ( $c['section_number'] ) {
-									array_push( $section_array['sub_sections'], $c['title'] );
-								}
-							}
-						}
-
-						array_push( $pagenav_sections, $section_array );
-					}
-				?>
-			<?php endif; ?>
-
-			<?php if ( $content_sections ) : ?>
-				<div class="content-table-of-contents__entry pagenav-content pagenav-content--fullwidth">
-					<?php foreach ( $content_sections as $k => $i ) : ?>
-						<?php if ( $i['content'] && $i['section_title'] ) : $k = $k + 1; $sub_sections = []; $o = 0; $p = 0; ?>
-							<div class="content-table-of-contents__section">
-								<div class="section-title">
-									<h2 id="section-<?php echo $k; ?>"><?php echo $k . '. ' . $i['section_title']; ?></h2>
-								</div>
-
-								<?php
-									foreach ( $i['content'] as $n => $c ) {
-										if ( $c['section_number'] ) {
-											$o++;
-
-											array_push( $sub_sections, $k . '.' . $o );
-										}
-									}
-								?>
-
-								<?php foreach ( $i['content'] as $n => $c ) : ?>
-
-									<?php
-										$sub_section_id = '';
-
-										if ( $c['section_number'] ) {
-											$p++;
-
-											$sub_section_id = 'content-' . $k . '-' . $p;
-										}
-									?>
-
-									<div class="section-content">
-										<?php if ( isset( $c['title'] ) && $c['title'] != '' ) : $n = $n + 1; ?>
-											<h4 id="<?php echo $sub_section_id; ?>"><?php if ( $c['section_number'] ) { echo $sub_sections[ $p - 1 ] . ' ' . $c['title']; } else { echo $c['title']; }; ?></h4>
-										<?php endif; ?>
-
-										<div class="section-content__entry">
-											<?php echo $c['entry']; ?>
-										</div>
-									</div>
-								<?php endforeach; ?>
-							</div>
-						<?php endif; ?>
-					<?php endforeach; ?>
-
-					<?php if ( $footnotes ) : ?>
-						<div class="content-footnotes">
-							<div class="footnotes">
-								<?php echo $footnotes; ?>
-							</div>
-						</div>
-					<?php endif; ?>
+			<?php if ( $footnotes ) : ?>
+				<div class="content-footnotes">
+					<div class="footnotes">
+						<?php echo $footnotes; ?>
+					</div>
 				</div>
 			<?php endif; ?>
 		</div>
