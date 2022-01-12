958eaf5929abf7400a7b3b42781f7e66f4fc73f1
Ash Saraga <ashsaraga@gmail.com>
2022-01-12 12:06:05 -0500

    Footnotes and CTA for Basic Text Template

--- wp-content/themes/oph/template/basic-text-template.php
+++ wp-content/themes/oph/template/basic-text-template.php
@@ -32,20 +32,18 @@
 	$call_to_action_button = get_field( 'call_to_action_button' );
 ?>
 
+<?php if ( ! empty( $call_to_action_button ) ) : ?>
 <div class="cta-button" id="button">
 	<div class="wrap">
 		<div class="cta-button__content">
 			<div class="button-group">
-				<?php if ( ! empty( $call_to_action_button ) ) : ?>
 					<?php foreach ( $call_to_action_button as $button ) : ?>
 						<a class="button" href="<?php echo $button['link']['url']; ?>"><?php echo $button['link']['title']; ?></a>
 					<?php endforeach; ?>
-				<?php else : ?>
-					<a class="button" href="/grants">Back to Grants Database</a>
-				<?php endif; ?>
 			</div>
 		</div>
 	</div>
 </div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
