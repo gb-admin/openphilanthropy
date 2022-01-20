14fb586b0fe279045e563a8ce4fd2ad7aaa1ab3f
Ash Saraga <ashsaraga@gmail.com>
2022-01-20 15:39:49 -0500

    Added 'Display Author' to Research Post Columns

--- wp-content/themes/oph/inc/helper-functions.php
+++ wp-content/themes/oph/inc/helper-functions.php
@@ -100,3 +100,30 @@ function oph_extract_custom_authors($author_list)
 
 	return $author_names;
 }
+
+// Adding 'Display Author' to Research Post List Columns 
+add_filter( 'manage_research_posts_columns', 'add_displayAuthor_column');
+/**
+ * Add new columns to the post table
+ *
+ * @param Array $columns - Current columns on the list post
+ */
+
+// Create New Column
+function add_displayAuthor_column( $columns ) { 
+  $columns['custom_author'] = __( 'Display Author' ); 
+  return $columns; 
+} 
+// Populate New Column
+add_action( 'manage_research_posts_custom_column', 'researchAuthor_column', 10, 2);
+function researchAuthor_column( $column, $post_id ) {
+  if ( 'custom_author' === $column ) {
+    $author = get_post_meta( $post_id, 'custom_author', true );
+
+    if ( ! $author ) {
+      _e( 'None' );  
+    } else {
+      echo $author;
+    }
+  } 
+}
