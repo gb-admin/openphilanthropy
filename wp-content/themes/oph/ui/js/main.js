9cd1a001d3ab16ec34d679924a4561d9130e9cb1
Chad Lamson <chad@greatbeliever.us>
2022-03-14 13:48:07 -0500

    console.log testing

--- wp-content/themes/oph/ui/js/main.js
+++ wp-content/themes/oph/ui/js/main.js
@@ -1361,7 +1361,7 @@ jQuery(function ($) {
       logme("Running genItem()");
       var itemList = "ul#" + itemID;
       logme(itemList);
-      console.log(item);
+      console.log(item.find("*:not(em)").contents().unwrap());
       $(itemList).append(
         $(
           '<li><a data-goto="#' +
