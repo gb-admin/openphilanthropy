ae379a7e407dd3fcad0d9a842c95bc384c244e1b
Ash Saraga <ashsaraga@gmail.com>
2022-03-31 15:23:37 -0400

    More Robust Categories Banner

--- wp-content/themes/oph/part/page-header-categories.php
+++ wp-content/themes/oph/part/page-header-categories.php
@@ -30,17 +30,22 @@ if ( !empty($filters) ) {
 				<nav aria-label="Filter Categories List">
 					<ul id="filter-categories-list">
 						<?php  
-						foreach( $filters as $key => $val ) {
-							$tax_slug = $val[0]; 
-							$tax_type = $key; 
-							$term = get_term_by('slug', $tax_slug, $tax_type); 
-							$query = $tax_type . '=' . $tax_slug; 
-							$escURL = str_replace($query, '', $theURL); 
-							?>
-							<li data-category="<?php echo $tax_type; ?>">
-								<a href="<?php echo $escURL; ?>"><?php echo $term->name; ?> <svg aria-hidden="true" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.857 6.506L1.571.221.157 1.635 6.443 7.92 0 14.363l1.414 1.415 6.443-6.443 6.442 6.442 1.415-1.414L9.27 7.92l6.285-6.285L14.142.221 7.857 6.506z"/></svg></a>
-							</li>
-						<?php
+						foreach( $filters as $key => $val ) { 
+							$i = 0; 
+							$items = count($val); 
+							while( $i < $items ) {
+								$tax_slug = $val[$i]; 
+								$tax_type = $key; 
+								$term = get_term_by('slug', $tax_slug, $tax_type); 
+								$query = $tax_type . '=' . $tax_slug; 
+								$escURL = str_replace($query, '', $theURL); 
+								?>
+								<li data-category="<?php echo $tax_type; ?>">
+									<a href="<?php echo $escURL; ?>"><?php echo $term->name; ?> <svg aria-hidden="true" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M7.857 6.506L1.571.221.157 1.635 6.443 7.92 0 14.363l1.414 1.415 6.443-6.443 6.442 6.442 1.415-1.414L9.27 7.92l6.285-6.285L14.142.221 7.857 6.506z"/></svg></a>
+								</li>
+							<?php 
+								 $i++;
+							}
 						}
 						?>
 					</ul>
