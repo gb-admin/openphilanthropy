78609f0598b5dd68fb52f3b59fd0167687504252
Ash Saraga <ashsaraga@gmail.com>
2022-04-07 15:44:00 -0400

    Checking to Exclude Empty Fields

--- wp-content/themes/oph/inc/helper-functions.php
+++ wp-content/themes/oph/inc/helper-functions.php
@@ -8,44 +8,46 @@
 function generate_filters( $slug, $type, $tax = false ) { 
   $params = get_url_params(); 
   foreach ( $type as $i ) { 
-    if ( $tax ) {
-      $ancestors = get_ancestors($i->term_id, $slug); 
-      $depth = count($ancestors) + 1; 
-      ?> 
-        <label 
-          for="<?php echo $i->name; ?>" 
-          class="selection-label" 
-          data-parent="<?php echo $i->parent ?>"
-          data-depth="level-<?php echo $depth; ?>"
-          data-termID="<?php echo $i->term_id ?>">
-          <input type="checkbox" 
-            class="selection-input" 
-            data-category="<?php echo $i->slug; ?>" 
-            data-termID="<?php echo $i->term_id ?>" 
-            id="<?php echo $i->name; ?>" 
-            name="<?php echo $slug; ?>"
-            value="<?php echo $i->slug; ?>" 
-            <?php if ( in_array( $i->slug, $params[$slug] ) ) { echo 'checked'; } ?> /> 
-          <span class="checked-box"></span>
-          <?php echo $i->name; ?>
-        </label> 
-      <?php
-    } else { 
-      ?> 
-        <label 
-          for="<?php echo $i; ?>" 
-          class="selection-label"> 
-          <input type="checkbox" 
-            class="selection-input" 
-            data-category="<?php echo $i; ?>" 
-            id="<?php echo $i; ?>" 
-            name="<?php echo $slug; ?>"
-            value="<?php echo $i; ?>" 
-            <?php if ( in_array( $i, $params[$slug] ) ) { echo 'checked'; } ?> /> 
-          <span class="checked-box"></span>
-          <?php echo $i; ?> 
-        </label> 
-      <?php
-    } 
+    if ( !empty( $i ) ) {
+      if ( $tax ) {
+        $ancestors = get_ancestors($i->term_id, $slug); 
+        $depth = count($ancestors) + 1; 
+        ?> 
+          <label 
+            for="<?php echo $i->name; ?>" 
+            class="selection-label" 
+            data-parent="<?php echo $i->parent ?>"
+            data-depth="level-<?php echo $depth; ?>"
+            data-termID="<?php echo $i->term_id ?>">
+            <input type="checkbox" 
+              class="selection-input" 
+              data-category="<?php echo $i->slug; ?>" 
+              data-termID="<?php echo $i->term_id ?>" 
+              id="<?php echo $i->name; ?>" 
+              name="<?php echo $slug; ?>"
+              value="<?php echo $i->slug; ?>" 
+              <?php if ( in_array( $i->slug, $params[$slug] ) ) { echo 'checked'; } ?> /> 
+            <span class="checked-box"></span>
+            <?php echo $i->name; ?>
+          </label> 
+        <?php
+      } else { 
+        ?> 
+          <label 
+            for="<?php echo $i; ?>" 
+            class="selection-label"> 
+            <input type="checkbox" 
+              class="selection-input" 
+              data-category="<?php echo $i; ?>" 
+              id="<?php echo $i; ?>" 
+              name="<?php echo $slug; ?>"
+              value="<?php echo $i; ?>" 
+              <?php if ( in_array( $i, $params[$slug] ) ) { echo 'checked'; } ?> /> 
+            <span class="checked-box"></span>
+            <?php echo $i; ?> 
+          </label> 
+        <?php
+      } 
+    }
   }
 } 
