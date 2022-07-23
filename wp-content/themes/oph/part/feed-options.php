28b16e1bbcde419b3bb73bc5e7a6e7d3fa04261f
markokukanjac <marko.kukanjac@toptal.com>
2022-07-23 15:06:36 +0200

    bugherd tasks

--- wp-content/themes/oph/part/feed-options.php
+++ wp-content/themes/oph/part/feed-options.php
@@ -1,17 +1,17 @@
 <?php
-	$post_type = '';
+$post_type = '';
 
-	if ( isset( $args['post_type'] ) ) {
-		$post_type = $args['post_type'];
-	}
+if (isset($args['post_type'])) {
+	$post_type = $args['post_type'];
+}
 
-	$sort_params = [
-		'a-z'              => 'A - Z',
-		'high-to-low'      => 'Highest to lowest',
-		'recent'           => 'Newest to oldest',
-		'oldest-to-newest' => 'Oldest to newest'
-	]; 
-	$sorted = $sort_params[$_GET['sort']];
+$sort_params = [
+	'a-z'              => 'A - Z',
+	'high-to-low'      => 'Highest to lowest',
+	'recent'           => 'Newest to oldest',
+	'oldest-to-newest' => 'Oldest to newest'
+];
+$sorted = $sort_params[$_GET['sort']];
 ?>
 
 <div class="feed-options-bar">
@@ -19,59 +19,68 @@
 		<nav aria-label="Feed Options Bar">
 			<div class="dropdown">
 				<button class="button" href="#">
-					Sort <?php if ( isset($_GET['sort']) ) { echo "({$sort_params[$_GET['sort']]}) &nbsp;"; } ?>
-					<svg viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.6 1.5l9.9 9.9 9.9-9.9" stroke="#6e7ca0" stroke-width="2"/></svg>
+					Sort <?php if (isset($_GET['sort'])) {
+								echo "({$sort_params[$_GET['sort']]}) &nbsp;";
+							} ?>
+					<svg viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M1.6 1.5l9.9 9.9 9.9-9.9" stroke="#6e7ca0" stroke-width="2" />
+					</svg>
 				</button>
 
 				<ul class="dropdown-content">
 					<li>
-						<a href="<?php echo esc_url( add_query_arg( 'sort', 'a-z' ) ); ?>#categories"><?= $sort_params['a-z']; ?></a> 
+						<a href="<?php echo esc_url(add_query_arg('sort', 'a-z')); ?>#categories"><?= $sort_params['a-z']; ?></a>
 					</li>
-					<?php if ( is_page_template( 'template/grants.php' ) ) : ?>
+					<?php if (is_page_template('template/grants.php')) : ?>
 						<li>
-							<a href="<?php echo esc_url( add_query_arg( 'sort', 'high-to-low' ) ); ?>#categories"><?= $sort_params['high-to-low']; ?></a>
+							<a href="<?php echo esc_url(add_query_arg('sort', 'high-to-low')); ?>#categories"><?= $sort_params['high-to-low']; ?></a>
 						</li>
 					<?php endif; ?>
 					<li>
-						<a href="<?php echo esc_url( add_query_arg( 'sort', 'recent' ) ); ?>#categories"><?= $sort_params['recent']; ?></a>
+						<a href="<?php echo esc_url(add_query_arg('sort', 'recent')); ?>#categories"><?= $sort_params['recent']; ?></a>
 					</li>
 					<li>
-						<a href="<?php echo esc_url( add_query_arg( 'sort', 'oldest-to-newest' ) ); ?>#categories"><?= $sort_params['oldest-to-newest']; ?></a>
+						<a href="<?php echo esc_url(add_query_arg('sort', 'oldest-to-newest')); ?>#categories"><?= $sort_params['oldest-to-newest']; ?></a>
 					</li>
-					<?php 
-						if ( isset($_GET['sort']) ) { 
-							$url = "https://$_SERVER[HTTP_HOST]/".strtok($_SERVER["REQUEST_URI"],'?'); 
-							?>
-							<li>
-								<a href="<?php echo $url; ?>">Clear Sorting</a>
-							</li>
-						<?php }
+					<?php
+					if (isset($_GET['sort'])) {
+						$url = "https://$_SERVER[HTTP_HOST]/" . strtok($_SERVER["REQUEST_URI"], '?');
+					?>
+						<li>
+							<a href="<?php echo $url; ?>">Clear Sorting</a>
+						</li>
+					<?php }
 					?>
 				</ul>
 			</div>
 
 			<div class="dropdown dropdown--inline-content">
 				<button class="button" href="#">
-					Items <?php if ( isset($_GET['items']) ) echo "({$_GET['items']})"; ?>
-					<svg viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.6 1.5l9.9 9.9 9.9-9.9" stroke="#6e7ca0" stroke-width="2"/></svg>
+					Items <?php if (isset($_GET['items'])) echo "({$_GET['items']})"; ?>
+					<svg viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M1.6 1.5l9.9 9.9 9.9-9.9" stroke="#6e7ca0" stroke-width="2" />
+					</svg>
 				</button>
 
 				<ul class="dropdown-content">
 					<li>
-						<a href="<?php echo esc_url( add_query_arg( 'items', '25' ) ); ?>#categories">25</a>
+						<a href="<?php echo esc_url(add_query_arg('items', '25')); ?>#categories">25</a>
 					</li>
 					<li>
-						<a href="<?php echo esc_url( add_query_arg( 'items', '50' ) ); ?>#categories">50</a>
+						<a href="<?php echo esc_url(add_query_arg('items', '50')); ?>#categories">50</a>
 					</li>
 					<li>
-						<a href="<?php echo esc_url( add_query_arg( 'items', '100' ) ); ?>#categories">100</a>
+						<a href="<?php echo esc_url(add_query_arg('items', '100')); ?>#categories">100</a>
 					</li>
 				</ul>
 			</div>
 
-			<?php if ( $post_type == 'research' || is_page_template( 'template/research-category.php' ) ) : ?>
+			<?php if ($post_type == 'research' || is_page_template('template/research-category.php') || is_page_template('template/grants.php')) : ?>
 				<button class="button button--solid button-view-list">
-					<?php echo oph_display_type('list'); ?>
+					<?php
+					$default_layout = is_page_template('template/grants.php') ? 'grid' : 'list';
+					echo oph_display_type($default_layout);
+					?>
 				</button>
 			<?php endif; ?>
 		</nav>
--- wp-content/themes/oph/template/home.php
+++ wp-content/themes/oph/template/home.php
@@ -1,96 +1,97 @@
 <?php
-	/**
-	 * Template Name: Home
-	 */
 
-	the_post();
+/**
+ * Template Name: Home
+ */
 
-	get_header();
+the_post();
 
-	$about_button = get_field( 'about_button' );
-	$about_content = get_field( 'about_content' );
-	$about_title = get_field( 'about_title' );
+get_header();
 
-	$homepage_grants = get_field( 'homepage_grants' );
-	$homepage_grants_button = get_field( 'homepage_grants_button' );
-	$homepage_grants_title = get_field( 'homepage_grants_title' );
+$about_button = get_field('about_button');
+$about_content = get_field('about_content');
+$about_title = get_field('about_title');
 
-	$homepage_research = get_field( 'homepage_research' );
-	$homepage_research_button = get_field( 'homepage_research_button' );
-	$homepage_research_title = get_field( 'homepage_research_title' );
+$homepage_grants = get_field('homepage_grants');
+$homepage_grants_button = get_field('homepage_grants_button');
+$homepage_grants_title = get_field('homepage_grants_title');
+
+$homepage_research = get_field('homepage_research');
+$homepage_research_button = get_field('homepage_research_button');
+$homepage_research_title = get_field('homepage_research_title');
 ?>
 
-<?php get_template_part( 'part/hero' ); ?>
+<?php get_template_part('part/hero'); ?>
 
-<?php if ( $homepage_grants ) : ?>
+<?php if ($homepage_grants) : ?>
 	<div id="highlighted-grants">
 		<div class="wrap">
-			<?php if ( $homepage_grants_title ) : ?>
+			<?php if ($homepage_grants_title) : ?>
 				<div class="line-heading">
 					<h2>Highlighted Grants</h2>
 				</div>
 			<?php endif; ?>
 
 			<ul class="list-3-col" id="highlighted-grants-list">
-				<?php foreach ( $homepage_grants as $grant ) : ?>
+				<?php foreach ($homepage_grants as $grant) : ?>
 
 					<?php
-						$grant_post = $grant['grant'][0];
+					$grant_post = $grant['grant'][0];
 
-						if ( $grant_post ) {
-							$focus_area = get_the_terms( $grant_post->ID, 'focus-area' );
+					if ($grant_post) {
+						$focus_area = get_the_terms($grant_post->ID, 'focus-area');
 
-							$grant_image = '';
+						$grant_image = '';
 
-							// Get category image
-							if ( ! empty( $focus_area ) && $focus_area[0]->term_id ) {
-								$grant_image = get_field( 'category_focus_areas_image', 'focus-area_' . $focus_area[0]->term_id );
-							}
-
-							// If category image found get url
-							if ( ! empty( $grant_image && $grant_image['sizes'] && $grant_image['sizes']['lg'] ) ) {
-								$grant_image = $grant_image['sizes']['lg'];
-							}
-
-							$grant_link = get_permalink( $grant_post->ID );
-							$grant_title = get_the_title( $grant_post->ID );
-
-							$grant_excerpt_source = get_the_excerpt( $grant_post->ID );
-
-							$grant_excerpt = array(
-								'append' => '...',
-								'limit' => 28,
-								'limitby' => 'word',
-								'source' => $grant_excerpt_source
-							);
-
-							$grant_description = excerpt( $grant_excerpt );
+						// Get category image
+						if (!empty($focus_area) && $focus_area[0]->term_id) {
+							$grant_image = get_field('category_focus_areas_image', 'focus-area_' . $focus_area[0]->term_id);
 						}
 
-						if ( $grant['description'] ) {
-							$grant_description = $grant['description'];
+						// If category image found get url
+						if (!empty($grant_image && $grant_image['sizes'] && $grant_image['sizes']['lg'])) {
+							$grant_image = $grant_image['sizes']['lg'];
 						}
 
-						if ( $grant['image'] ) {
-							$grant_image = $grant['image']['sizes']['lg'];
-						}
+						$grant_link = get_permalink($grant_post->ID);
+						$grant_title = get_the_title($grant_post->ID);
 
-						if ( $grant['link'] ) {
-							$grant_link = $grant['link']['url'];
-						}
+						$grant_excerpt_source = get_the_excerpt($grant_post->ID);
 
-						if ( $grant['title'] ) {
-							$grant_title = $grant['title'];
-						}
+						$grant_excerpt = array(
+							'append' => '...',
+							'limit' => 28,
+							'limitby' => 'word',
+							'source' => $grant_excerpt_source
+						);
+
+						$grant_description = excerpt($grant_excerpt);
+					}
+
+					if ($grant['description']) {
+						$grant_description = $grant['description'];
+					}
+
+					if ($grant['image']) {
+						$grant_image = $grant['image']['sizes']['lg'];
+					}
+
+					if ($grant['link']) {
+						$grant_link = $grant['link']['url'];
+					}
+
+					if ($grant['title']) {
+						$grant_title = $grant['title'];
+					}
 					?>
 
-					<?php if ( $grant_title ) : ?>
+					<?php if ($grant_title) : ?>
 						<li>
 							<a href="<?php echo $grant_link; ?>">
-								<?php if ( $grant_image ) : ?>
+								<?php if ($grant_image) : ?>
 									<div class="bucket-image">
 										<div class="bucket-image__bg">
-											<img src="<?php echo $grant_image; ?>" alt="<?php echo $focus_area; ?>"> 
+											<img src="<?php echo $grant_image; ?>" alt="<?php echo $focus_area; ?>">
 										</div>
 									</div>
 								<?php endif; ?>
@@ -106,11 +107,13 @@
 				<?php endforeach; ?>
 			</ul>
 
-			<?php if ( $homepage_grants_button ) : ?>
+			<?php if ($homepage_grants_button) : ?>
 				<div class="button-group">
-					<?php foreach ( $homepage_grants_button as $i ) : ?>
-						<?php if ( $i['link']['url'] ) : ?>
-							<a class="button" href="<?php echo $i['link']['url']; ?>"<?php if ( $i['link']['target'] == '_blank' ) { echo ' target="_blank"'; } ?>><?php echo $i['link']['title']; ?></a>
+					<?php foreach ($homepage_grants_button as $i) : ?>
+						<?php if ($i['link']['url']) : ?>
+							<a class="button" href="<?php echo $i['link']['url']; ?>" <?php if ($i['link']['target'] == '_blank') {
+																							echo ' target="_blank"';
+																						} ?>><?php echo $i['link']['title']; ?></a>
 						<?php endif; ?>
 					<?php endforeach; ?>
 				</div>
@@ -121,22 +124,24 @@
 
 <div id="about">
 	<div class="wrap">
-		<?php if ( $about_title ) : ?>
+		<?php if ($about_title) : ?>
 			<div class="line-heading">
 				<h2><?php echo $about_title; ?></h2>
 			</div>
 		<?php endif; ?>
 
 		<div class="about-content">
-			<?php if ( $about_content ) : ?>
+			<?php if ($about_content) : ?>
 				<?php echo $about_content; ?>
 			<?php endif; ?>
 
-			<?php if ( $about_button ) : ?>
+			<?php if ($about_button) : ?>
 				<div class="button-group">
-					<?php foreach ( $about_button as $i ) : ?>
-						<?php if ( $i['link']['url'] ) : ?>
-							<a class="button" href="<?php echo $i['link']['url']; ?>"<?php if ( $i['link']['target'] == '_blank' ) { echo ' target="_blank"'; } ?>><?php echo $i['link']['title']; ?></a>
+					<?php foreach ($about_button as $i) : ?>
+						<?php if ($i['link']['url']) : ?>
+							<a class="button" href="<?php echo $i['link']['url']; ?>" <?php if ($i['link']['target'] == '_blank') {
+																							echo ' target="_blank"';
+																						} ?>><?php echo $i['link']['title']; ?></a>
 						<?php endif; ?>
 					<?php endforeach; ?>
 				</div>
@@ -145,10 +150,10 @@
 	</div>
 </div>
 
-<?php if ( $homepage_research ) : ?>
+<?php if ($homepage_research) : ?>
 	<div id="research-preview">
 		<div class="wrap">
-			<?php if ( $homepage_research_title ) : ?>
+			<?php if ($homepage_research_title) : ?>
 				<div class="line-heading">
 					<h2><?php echo $homepage_research_title; ?></h2>
 				</div>
@@ -156,89 +161,99 @@
 
 			<div class="research-preview-content">
 				<ul class="list-related-posts list-3-col" id="research-preview-list">
-					<?php foreach ( $homepage_research as $i ) : ?>
+					<?php foreach ($homepage_research as $i) : ?>
 
 						<?php
-							$research_post = $i['post'][0];
+						$research_post = $i['post'][0];
 
-							if ( $research_post ) {
-								$research_content_type = get_the_terms( $research_post->ID, 'content-type' );
+						if ($research_post) {
+							$research_content_type = get_the_terms($research_post->ID, 'content-type');
 
-								$linkExternally = get_field('externally_link', $research_post->ID); 
-								if( $linkExternally ){
-									$research_link = get_field('external_url', $research_post->ID); 
-								}else{
-									$research_link = get_permalink( $research_post->ID );
-								}
-
-								$research_title = get_the_title( $research_post->ID );
-
-								$research_excerpt_source = get_the_excerpt( $research_post->ID );
-
-								$research_excerpt = array(
-									'append' => '...',
-									'limit' => 38,
-									'limitby' => 'word',
-									'source' => $research_excerpt_source
-								);
-
-								$research_description = excerpt( $research_excerpt );
-							}
-		
-							if ( $research_content_type ) {
-								$first_parent_term = get_term_top_most_parent($research_content_type[0],'content-type');
-								$research_eyebrow_copy = $first_parent_term->name;
-								$research_eyebrow_link = get_term_link($first_parent_term);
-
-								// Top level content types uses document root, remove the standard content-type path
-								$research_eyebrow_link = str_replace("content-type/", '', $research_eyebrow_link);
+							$linkExternally = get_field('externally_link', $research_post->ID);
+							if ($linkExternally) {
+								$research_link = get_field('external_url', $research_post->ID);
+							} else {
+								$research_link = get_permalink($research_post->ID);
 							}
 
-							if ( $i['description'] ) {
-								$research_description = $i['description'];
-							}
+							$research_title = get_the_title($research_post->ID);
 
-							if ( $i['eyebrow_copy'] ) {
-								$research_eyebrow_copy = $i['eyebrow_copy'];
-							}
+							$research_excerpt_source = get_the_excerpt($research_post->ID);
 
-							if ( $i['eyebrow_link'] ) {
-								$research_eyebrow_copy = $i['eyebrow_link'];
-							}
+							$research_excerpt = array(
+								'append' => '...',
+								'limit' => 38,
+								'limitby' => 'word',
+								'source' => $research_excerpt_source
+							);
 
-							if ( $i['image'] ) {
-								$research_image = $i['image']['sizes']['lg'];
-							}
+							$research_description = excerpt($research_excerpt);
+						}
 
-							if ( $i['link'] ) {
-								$research_link = $i['link']['url'];
-							}
+						if ($research_content_type) {
+							$first_parent_term = get_term_top_most_parent($research_content_type[0], 'content-type');
+							$research_eyebrow_copy = $first_parent_term->name;
+							$research_eyebrow_link = get_term_link($first_parent_term);
 
-							if ( $i['title'] ) {
-								$research_title = $i['title'];
-							}
+							// Top level content types uses document root, remove the standard content-type path
+							$research_eyebrow_link = str_replace("content-type/", '', $research_eyebrow_link);
+						}
+
+						if ($i['description']) {
+							$research_description = $i['description'];
+						}
+
+						if ($i['eyebrow_copy']) {
+							$research_eyebrow_copy = $i['eyebrow_copy'];
+						}
+
+						if ($i['eyebrow_link']) {
+							$research_eyebrow_copy = $i['eyebrow_link'];
+						}
+
+						if ($i['image']) {
+							$research_image = $i['image']['sizes']['lg'];
+						}
+
+						if ($i['link']) {
+							$research_link = $i['link']['url'];
+						}
+
+						if ($i['title']) {
+							$research_title = $i['title'];
+						}
 						?>
 
-						<li<?php if ( ! $research_eyebrow_copy ) { echo ' class="has-no-eyebrow-copy"'; } ?>>
-							<h5><?php if ( $research_eyebrow_link ) { echo '<a href="' . $research_eyebrow_link . '">'; } echo $research_eyebrow_copy; if ( $research_eyebrow_link ) { echo '</a>'; } ?></h5>
+						<li<?php if (!$research_eyebrow_copy) {
+								echo ' class="has-no-eyebrow-copy"';
+							} ?>>
+							<h5><?php if ($research_eyebrow_link) {
+									echo '<a href="' . $research_eyebrow_link . '">';
+								}
+								echo $research_eyebrow_copy;
+								if ($research_eyebrow_link) {
+									echo '</a>';
+								} ?></h5>
 
 							<h4>
 								<a href="<?php echo $research_link; ?>"><?php echo $research_title; ?></a>
 							</h4>
 
-							<?php if ( $research_description ) : ?>
+							<?php if ($research_description) : ?>
 								<div class="single-related-posts__description">
 									<?php echo $research_description; ?>
 								</div>
 							<?php endif; ?>
 
 							<div class="single-related-posts__link">
-								<a href="<?php echo $research_link; ?>"> 
-									Read More <svg viewBox="0 0 25 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.352 1l7.395 7.5-7.395 7.5M1 8.397l21.748.103" stroke="#6e7ca0" stroke-width="2"/></svg>
+								<a href="<?php echo $research_link; ?>">
+									Read More <svg viewBox="0 0 25 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<path d="M15.352 1l7.395 7.5-7.395 7.5M1 8.397l21.748.103" stroke="#6e7ca0" stroke-width="2" />
+									</svg>
 								</a>
 							</div>
-						</li>
-					<?php endforeach; ?>
+							</li>
+						<?php endforeach; ?>
 				</ul>
 
 				<div class="button-group">
