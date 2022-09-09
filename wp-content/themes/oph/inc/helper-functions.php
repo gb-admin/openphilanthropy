02f8351388bf1adcd789ecc4401d8b2cff027fee
markokukanjac <marko.kukanjac@toptal.com>
2022-09-09 15:26:33 +0200

    filter fixes

--- wp-content/themes/oph/inc/helper-functions.php
+++ wp-content/themes/oph/inc/helper-functions.php
@@ -8,7 +8,7 @@
 function generate_filters( $slug, $type, $tax = false, $allowed = false ) { 
   $params = get_url_params();
   $slug = $slug;
-  foreach ( $type as $i ) { 
+  foreach ( $type as $i ) {
     if ( !empty( $i ) ) {
       if ( $tax ) {
         $ancestors = get_ancestors($i->term_id, $slug); 
@@ -54,7 +54,7 @@ function generate_filters( $slug, $type, $tax = false, $allowed = false ) {
               data-category="<?php echo $i->slug; ?>" 
               data-termID="<?php echo $i->term_id ?>" 
               id="<?php echo $i->name; ?>" 
-              name="<?php echo $slug; ?>"
+              name="<?php echo $slug; ?>[]"
               value="<?php echo $i->slug; ?>" 
               <?php if ( $params[$slug] && in_array( $i->slug, $params[$slug] ) ) { echo 'checked'; } ?> /> 
             <span class="checked-box"></span>
@@ -63,7 +63,7 @@ function generate_filters( $slug, $type, $tax = false, $allowed = false ) {
           <?php 
           }
         }
-      } else { 
+      } else {
         ?> 
           <label 
             for="<?php echo $i; ?>" 
@@ -72,9 +72,17 @@ function generate_filters( $slug, $type, $tax = false, $allowed = false ) {
               class="selection-input" 
               data-category="<?php echo $i; ?>" 
               id="<?php echo $i; ?>" 
-              name="<?php echo $slug; ?>"
+              name="<?php echo $slug; ?>[]"
               value="<?php echo $i; ?>" 
-              <?php if ( $params[$slug] && in_array( $i, $params[$slug] ) ) { echo 'checked'; } ?> /> 
+              <?php
+              if ( !empty($params) ) {
+                foreach($params as $param) {
+                  if ( in_array( $i, $param )) {
+                    echo 'checked';
+                  }
+                }
+              }
+              ?> /> 
             <span class="checked-box"></span>
             <?php echo $i; ?> 
           </label> 
