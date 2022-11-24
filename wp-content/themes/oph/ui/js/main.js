3178ec70615317e6a08d5b5525ab0eea3a0a86a4
John_B <john@birchall.info>
2022-11-24 12:26:13 +0000

    change double to single quotes in main.js to deal with unrecognized expression error

--- wp-content/themes/oph/ui/js/main.js
+++ wp-content/themes/oph/ui/js/main.js
@@ -1725,7 +1725,7 @@ jQuery(function ($) {
         var target = $(this.hash);
         target = target.length
           ? target
-          : $("[name=" + this.hash.slice(1) + "]");
+          : $("[name=' + this.hash.slice(1) + ']");
 
         if (target.length) {
           $("html,body").animate(
