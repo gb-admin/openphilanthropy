7874ba8b3f4f1bcfa0f185675e32f0264853311b
John_B <john@birchall.info>
2023-03-10 09:21:13 +0000

    remove undefined oph_posted_on and oph_posted_by

--- wp-content/themes/oph/part/content-search.php
+++ wp-content/themes/oph/part/content-search.php
@@ -22,13 +22,6 @@
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( $url ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			oph_posted_on();
-			oph_posted_by();
-			?>
-		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
--- wp-content/themes/oph/part/content.php
+++ wp-content/themes/oph/part/content.php
@@ -18,15 +18,7 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="entry-meta">
-				<?php
-				oph_posted_on();
-				oph_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<?php the_post_thumbnail();  ?>
